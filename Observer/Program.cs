576a2d6ffe2c04439486a802cc1126a61d406ee4
yemregunduz <yegunduz@outlook.com>
2022-04-15 01:27:26 +0300

    Observer and Strategy design patterns added.

---
+++ Observer/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Observer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var customerObserver = new CustomerObserver();
+            var employeeObserver = new EmployeeObserver();
+            ProductManager productManager = new();
+            productManager.Attach(customerObserver);
+            productManager.Attach(employeeObserver);
+            productManager.UpdatePrice();
+            Console.WriteLine("-------AFTER DETACH CUSTOMER OBSERVER---------");
+            productManager.Detach(customerObserver);
+            productManager.UpdatePrice();
+            Console.ReadLine();
+        }
+        class ProductManager
+        {
+            List<Observer> _observers = new List<Observer>();
+            public void UpdatePrice()
+            {
+                Console.WriteLine("Product price updated.");
+                Notify();
+            }
+            public void Attach(Observer observer)
+            {
+                _observers.Add(observer);
+            }
+            public void Detach(Observer observer)
+            {
+                _observers.Remove(observer);
+            }
+            private void Notify()
+            {
+                foreach (var observer in _observers)
+                {
+                    observer.Update();
+                }
+            }
+        }
+        abstract class Observer
+        {
+            public abstract void Update();
+        }
+        class CustomerObserver : Observer
+        {
+            public override void Update()
+            {
+                Console.WriteLine("Message to customer: Product price changed!");
+            }
+        }
+        class EmployeeObserver : Observer
+        {
+            public override void Update()
+            {
+                Console.WriteLine("Message to employee: Product price changed!");
+            }
+        }
+    }
+}
---
+++ Strategy/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+namespace Strategy
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new();
+            customerManager.CreditCalculatorBase = new PersonalFinanceCredit();
+            customerManager.SaveCredit();
+
+            Console.WriteLine("-----------");
+
+            customerManager.CreditCalculatorBase = new TransportCredit();
+            customerManager.SaveCredit();
+        }
+    }
+    abstract class CreditCalculatorBase
+    {
+        public abstract void Calculate();
+    }
+    class PersonalFinanceCredit : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Personal finance credit calculated");
+        }
+    }
+    class TransportCredit : CreditCalculatorBase
+    {
+        public override void Calculate()
+        {
+            Console.WriteLine("Transport credit calculated");
+        }
+    }
+    class CustomerManager
+    {
+        public CreditCalculatorBase CreditCalculatorBase { get; set; }
+        public void SaveCredit()
+        {
+            //Business code..
+            CreditCalculatorBase.Calculate();
+
+        }
+    }
+}
