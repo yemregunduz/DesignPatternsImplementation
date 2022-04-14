f3de0e7035c9791e02e42025679c7d4ffd7f564f
yemregunduz <yegunduz@outlook.com>
2022-04-15 00:16:12 +0300

    ConsoleApp1 project deleted.

---
+++ Adapter/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProductManager productManagerWithAdapterLogger = new ProductManager(new YtuLoggerAdapter());
+            productManagerWithAdapterLogger.Save();
+            Console.WriteLine("----------");
+            ProductManager productManagerWithYegLogger = new ProductManager(new YegLogger());
+            productManagerWithYegLogger.Save();
+            Console.ReadLine();
+        }
+    }
+    class ProductManager
+    {
+        ILogger _logger;
+
+        public ProductManager(ILogger logger)
+        {
+            _logger = logger;
+        }
+
+        public void Save()
+        {
+            _logger.Log("Product Data");
+            Console.WriteLine("Saved");
+        }
+    }
+    interface ILogger
+    {
+        void Log(string message);
+    }
+    class YegLogger : ILogger
+    {
+        public void Log(string message)
+        {
+            Console.WriteLine(message+" Logged with YegLogger");
+        }
+    }
+    //Nuget package
+    class YtuLogger
+    {
+        public void LogMessage(string message)
+        {
+            Console.WriteLine(message + " Logged with YtuLogger");
+        }
+    }
+    class YtuLoggerAdapter : ILogger
+    {
+        public void Log(string message)
+        {
+            YtuLogger logger = new YtuLogger();
+            logger.LogMessage(message);
+        }
+    }
+}
---
+++ Composite/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Composite
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee yunusEmre = new () { Name="Yunus Emre Gündüz"};
+            Employee fatmaNur = new () { Name = "Fatma Nur Karaman" };
+            fatmaNur.AddSubordinate(yunusEmre);
+
+            Employee fatihMehmet = new () { Name = "Fatih Mehmet Gündüz" };
+            fatmaNur.AddSubordinate(fatihMehmet);
+
+            Employee selimEge = new() { Name = "Selim Ege Çırpıcı" };
+            yunusEmre.AddSubordinate(selimEge);
+
+            Supplier aliCan = new() { Name = "Ali Can Çırpıcı" };
+            fatihMehmet.AddSubordinate(aliCan);
+
+            Console.WriteLine(fatmaNur.Name);
+            foreach (Employee manager in fatmaNur)
+            {
+                Console.WriteLine("  -"+manager.Name);
+                foreach (IPerson employee in manager)
+                {
+                    Console.WriteLine("    -"+employee.Name);
+                }
+            }
+            Console.ReadLine();
+        }
+        interface IPerson
+        {
+             string Name { get; set; }
+        }
+        class Supplier : IPerson
+        {
+            public string Name { get; set;  }
+        }
+        class Employee : IPerson, IEnumerable<IPerson>
+        {
+            private List<IPerson> _subordinates = new List<IPerson>(); 
+            public void AddSubordinate(IPerson person)
+            {
+                _subordinates.Add(person);
+            }
+            public void RemoveSubordinates(IPerson person)
+            {
+                _subordinates.Remove(person);
+            }
+            public IPerson GetSubOrdinate(int index)
+            {
+                return _subordinates[index];
+            }
+            public string Name { get; set; }
+
+            public IEnumerator<IPerson> GetEnumerator()
+            {
+                foreach (var subordinate in _subordinates)
+                {
+                    yield return subordinate;
+                }
+            }
+
+            IEnumerator IEnumerable.GetEnumerator()
+            {
+                return GetEnumerator();
+            }
+        }
+    }
+}
