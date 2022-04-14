a4c3a894fda9014db2576f3c635594cc81a41917
yemregunduz <yegunduz@outlook.com>
2022-04-15 01:05:33 +0300

    Bridge pattern added.

---
+++ Bridge/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace Bridge
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CustomerManager customerManager = new();
+            customerManager.MessageSenderBase = new EmailSender();
+            customerManager.UpdateCustomer();
+
+            Console.WriteLine("-------------------");
+
+            customerManager.MessageSenderBase = new SmsSender();
+            customerManager.UpdateCustomer();
+            Console.ReadLine();
+        }
+    }
+    class CustomerManager
+    {
+        public MessageSenderBase MessageSenderBase { get; set; }
+        public void UpdateCustomer()
+        {
+            MessageSenderBase.Send(new Body() { Title="About the C#"});
+            Console.WriteLine("Customer updated.");
+        }
+    }
+    abstract class MessageSenderBase
+    {
+        public void Save()
+        {
+            Console.WriteLine("Mesage saved");
+        }
+        public abstract void Send(Body body);
+    }
+    class Body
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+    }
+    class EmailSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via Email sender", body.Title);
+        }
+    }
+    class SmsSender : MessageSenderBase
+    {
+        public override void Send(Body body)
+        {
+            Console.WriteLine("{0} was sent via Sms sender", body.Title);
+        }
+    }
+}
