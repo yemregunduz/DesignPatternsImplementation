d25a2ac7ddc9da8a83587f593c7958cbf53d7e43
yemregunduz <yegunduz@outlook.com>
2022-04-15 05:18:27 +0300

    Multiton design pattern added.

---
+++ Multiton/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Multiton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Camera nikonCamera1 = Camera.GetCamera("Nikon");
+            Camera nikonCamera2 = Camera.GetCamera("Nikon");
+            Camera canonCamera1 = Camera.GetCamera("Canon");
+            Camera canonCamera2 = Camera.GetCamera("Canon");
+            Console.WriteLine(canonCamera1.Id);
+            Console.WriteLine(canonCamera2.Id);
+            Console.WriteLine(nikonCamera1.Id);
+            Console.WriteLine(nikonCamera2.Id);
+        }
+    }
+    class Camera
+    {
+        static Dictionary<string, Camera> _cameras = new();
+        static object _lock = new();
+        public Guid Id { get; set; }
+        private Camera()
+        {
+            //Test
+            Id = Guid.NewGuid();
+        }
+        public static Camera GetCamera(string brand)
+        {
+            lock (_lock)
+            {
+                if (!_cameras.ContainsKey(brand))
+                {
+                    _cameras.Add(brand, new Camera());
+                }
+            }
+            return _cameras[brand];
+        }
+    }
+}
