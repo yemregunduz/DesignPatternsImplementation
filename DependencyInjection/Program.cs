516a1aaaca5fe62eb2316ce6a81623bd8b3395c4
yemregunduz <yegunduz@outlook.com>
2022-04-15 04:55:34 +0300

    Dependency Injection design pattern add.

---
+++ DependencyInjection/Program.cs
@@ -0,0 +1,50 @@
+﻿using Ninject;
+using System;
+
+namespace DependencyInjection
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKernel kernel = new StandardKernel();
+            //kernel.Bind<IProductDal>().To<EfProductDal>().InSingletonScope();
+            kernel.Bind<IProductDal>().To<NhProductDal>().InSingletonScope();
+            ProductManager productManager = new(kernel.Get<IProductDal>());
+            productManager.Save();
+        }
+    }
+    interface IProductDal
+    {
+        public void Save();
+    }
+    class EfProductDal:IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Entity Framework");
+        }
+    }
+    class NhProductDal : IProductDal
+    {
+        public void Save()
+        {
+            Console.WriteLine("Saved with Nhibernate");
+        }
+    }
+    class ProductManager
+    {
+        private readonly IProductDal _productDal;
+
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+
+        public void Save()
+        {
+            //Business code 
+            _productDal.Save();
+        }
+    }
+}
